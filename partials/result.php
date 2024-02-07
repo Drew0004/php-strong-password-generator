4a2928b61564133ce08cccf31636976250e711d4
Drew0004 <drdrewgraphic@gmail.com>
2024-02-07 16:22:51 +0100

    add logout page milestone 3

--- partials/result.php
+++ partials/result.php
@@ -23,6 +23,11 @@
                     echo $_SESSION['finalpassword']
                 ?>
             </h3>
+            <div class="text-center">
+                <button class="btn btn-primary my-3">
+                    <a class="text-white text-decoration-none" href="./logout.php">Torna alla home</a>
+                </button>
+            </div>
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
